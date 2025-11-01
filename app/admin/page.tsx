98c88ca3d21cd2bd9a6d83d32a1a16b883bf83df
ngouhouodonald <ngouhouodonald@gmail.com>
2025-11-01 11:15:46 +0100

    'dernier'

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -19,7 +19,7 @@ import {
 } from 'lucide-react';
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL ;
 
 interface Product {
   _id: string;
